ca1e9222b3a55fcfd082deb4f63ffd57ace0723f
atofinite <bhargav.jagdish@kalvium.community>
2025-04-16 14:03:25 +0530

    Implemented relationship between User and Expense models

--- Backend/routes/Expense.js
+++ Backend/routes/Expense.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 
 const expenseSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
   category: { type: String, required: true },
   amount: { type: Number, required: true },
   date: { type: Date, required: true },
-  notes: { type: String },
+  notes: { type: String }
 });
 
 module.exports = mongoose.model('Expense', expenseSchema);
